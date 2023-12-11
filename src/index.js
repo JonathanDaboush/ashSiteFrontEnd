6f8534e56d590fac4562b70495d942bebd953ea8
Jonathan Daboush <90986058+JonathanDaboush@users.noreply.github.com>
2023-12-11 16:21:52 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import jsog from 'jsog';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
